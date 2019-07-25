f38890f276b4b13217459c7ee543d7efa5729845
klaus <oriokiklaus@gmail.com>
2019-07-25 17:20:51 +0300

    add bounding box for the contour to obtain aspect ratio and identify the radio buttons by their circular shape within the form

--- backend/health_app/scanner.py
+++ backend/health_app/scanner.py
@@ -70,3 +70,64 @@ thresh = cv2.threshold(warped, 0, 255,
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
+questionCnts = []
+
+for c in cnts:
+	# compute the bounding box of the contour, then use the
+	# bounding box to derive the aspect ratio
+
+	(x, y, w, h) = cv2.boundingRect(c)
+	ar = w / float(h)
+ 
+	# in order to label the contour as a question, region
+	# should be sufficiently wide, sufficiently tall, and
+	# have an aspect ratio approximately equal to 1
+	if w >= 10 and h >= 10 and ar >= 0.6 and ar <= 1.1:
+		questionCnts.append(c)
+
+# sort the question contours top-to-bottom, then initialize
+# the total number of correct answers
+questionCnts = contours.sort_contours(questionCnts,
+	method="top-to-bottom")[0]
+correct = 0
+ 
+# each question has 5 possible answers, to loop over the
+# question in batches of 5
+for (q, i) in enumerate(np.arange(0, len(questionCnts), 2)):
+	# sort the contours for the current question from
+	# left to right, then initialize the index of the
+	# bubbled answer
+	cnts = contours.sort_contours(questionCnts[i:i + 5])[0]
+	bubbled = None
+
+	# loop over the sorted contours
+	for (j, c) in enumerate(cnts):
+		# construct a mask that reveals only the current
+		# "bubble" for the question
+		mask = np.zeros(thresh.shape, dtype="uint8")
+		cv2.drawContours(mask, [c], -1, 255, -1)
+ 
+		# apply the mask to the thresholded image, then
+		# count the number of non-zero pixels in the
+		# bubble area
+		mask = cv2.bitwise_and(thresh, thresh, mask=mask)
+		total = cv2.countNonZero(mask)
+ 
+		# if the current total has a larger number of total
+		# non-zero pixels, then we are examining the currently
+		# bubbled-in answer
+		if bubbled is None or total > bubbled:
+			bubbled = (total, j)
+
+	# initialize the contour color and the index of the
+	# *correct* answer
+	color = (0, 0, 255)
+	k = ANSWER_KEY[q]
+ 
+	# check to see if the bubbled answer is correct
+	if k == bubbled[1]:
+		color = (0, 255, 0)
+		correct += 1
+ 
+	# draw the outline of the correct answer on the test
+	# cv2.drawContours(paper, [cnts[k]], -1, color, 3)
